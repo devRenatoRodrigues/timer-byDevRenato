4c5cbce6190ff0359506d8d23fc7bc416ed42bcd
devRenatoRodrigues <rehrlz@hotmail.com>
2023-07-25 18:06:15 -0300

    window alert with sweetAlert created

--- src/hooks/useCountDownTimer.jsx
+++ src/hooks/useCountDownTimer.jsx
@@ -1,8 +1,9 @@
-import cafeAudio from '../assets/queroCafe.mp3' 
+import cafeAudio from '../assets/depression.mp3' 
+import Swal from 'sweetalert2'
 import { useCallback, useEffect, useState } from 'react'
 
 function CountDownTimer() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(5);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
 
@@ -14,9 +15,27 @@ function CountDownTimer() {
   const playCafe = () => {
     const audioElement = new Audio(cafeAudio)
     audioElement.play()
+    Swal.fire({
+      title: 'CoffeBreak is over!!',
+      text: 'Back to the Office, Now!!',
+      imageUrl: 'src/assets/sales-lessons-from-michael-scott.jpg',
+      background: '#010D12',
+      color: '#FFFFFF',
+      confirmButtonColor: '#0d6efd',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Custom image',
+      customClass: {
+        container: 'custom-swal',
+      },
+    })
   }
+
+
   const clearButton = () => {
-    return setCounter(0);
+    setCounter(0);
+    setIsTimerRunning(false)
+    clearInterval(intervalId);
   };
 
   const stopTimer = useCallback(() => {
